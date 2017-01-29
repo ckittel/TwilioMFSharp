47b22fc0517ee2b723d9ca62bcb56bbf2c958e6e
Chad Kittel <chad.kittel@gmail.com>
2017-01-29 11:41:44 -0600

    Prepping for NuGet publish

--- TwilioMFSharp/Properties/AssemblyInfo.cs
+++ TwilioMFSharp/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Reflection;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.7.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
